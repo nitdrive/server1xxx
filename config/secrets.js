e42b4aac6dd4a720db2aa6d8df4be1c2c0659e95
Nithin <nithin.ayiluri@gmail.com>
2015-08-06 06:16:16 -0500

    Update secrets.js

--- config/secrets.js
+++ config/secrets.js
@@ -1,8 +1,8 @@
 module.exports = {
   
-  db: process.env.MONGODB || 'mongodb://localhost:27017/schoolmateDB',
+  //db: process.env.MONGODB || 'mongodb://localhost:27017/schoolmateDB',
    //db: process.env.MONGODB || 'mongodb://localhost:27017/nodelabs',
-  //db: process.env.MONGODB || 'mongodb://admin:schoolmate123@ds029803.mongolab.com:29803/schoolmatedb1',
+  db: process.env.MONGODB || 'mongodb://admin:schoolmate123@ds029803.mongolab.com:29803/schoolmatedb1',
   cryptos: {
     algorithm: 'aes256',
     key: process.env.CRYPTO_KEY || 'MAX1234'
